49d41536eed245b3bb047778af33f12a7bdd6861
unknown <dorian.koepfle@gmail.com>
2018-05-02 12:45:52 +0200

    push funktioniert

--- src/mystack.c
+++ src/mystack.c
@@ -6,16 +6,21 @@
 * instantiates a new stack and returns a pointer to it.
 * must be free'ed using stack_delete(...)
 */
-MyStack* stack_create(){
-  MyStack* stack = (MyStack*)malloc(sizeof(MyStack));
-  stack->top = -1;
-  return stack;
+MyStack* stack_create() {
+	MyStack* stack = (MyStack*)malloc(sizeof(MyStack));
+	stack->top = -1;
+	for (int i = 0; i < MAX_STACK_SIZE; i++)
+	{
+		stack->data[i] = 0;
+	}
+
+	return stack;
 }
 
 /*
 * deletes a given stack
 */
-void stack_delete(MyStack* stack){
+void stack_delete(MyStack* stack) {
 
 }
 
@@ -24,8 +29,19 @@ void stack_delete(MyStack* stack){
 * returns true if the push was successful,
 * returns false if not (the stack is full)
 */
-bool stack_push(MyStack* pStack, int data){
-  return false;
+bool stack_push(MyStack* pStack, int data) {
+
+	int newTop = (pStack->top) + 1;
+
+	if (pStack->top < MAX_STACK_SIZE-1) {
+
+		pStack->data[newTop] = data;
+		pStack->top = newTop;
+
+		return true;
+	}
+
+	return false;
 }
 
 /*
@@ -34,8 +50,8 @@ bool stack_push(MyStack* pStack, int data){
 * returns true if something was pop'ed
 * returns false if the stack was empty
 */
-bool stack_pop(MyStack* pStack, int* value){
-  return false;
+bool stack_pop(MyStack* pStack, int* value) {
+	return false;
 }
 
 /*
@@ -44,13 +60,13 @@ bool stack_pop(MyStack* pStack, int* value){
 * returns true if the stack was not empty
 * returns false if the stack was empty
 */
-bool stack_peek(MyStack* stack, int* value){
-  return false;
+bool stack_peek(MyStack* stack, int* value) {
+	return false;
 }
 
 /*
 * returns true if the stack is empty, otherwhise false
 */
-bool stack_isEmpty(MyStack* stack){
-  return false;
+bool stack_isEmpty(MyStack* stack) {
+	return false;
 }
--- test/test_mystack.c
+++ test/test_mystack.c
@@ -22,11 +22,50 @@ void test_stack_create(void)
   }
 }
 
+void test_stack_push_onStackWithOneElement(void) {
+	//given an empty stack
+	stack->data[0] = 1;
+	stack->top = 0;
+
+	//when
+	bool result= stack_push(stack, 2);
+
+
+	//then
+	TEST_ASSERT_EQUAL_INT(1, stack->top);
+	TEST_ASSERT_EQUAL_INT(2, stack->data[1]);
+
+	TEST_ASSERT_EQUAL_INT(1, stack->data[0]);
+	for (int i = 2; i < MAX_STACK_SIZE; i++)
+	{
+		TEST_ASSERT_EQUAL_INT(0, stack->data[i]);
+	}
+
+	TEST_ASSERT_TRUE(result);
+
+}
+
+void test_stack_push_stackIsFull(void) {
+	//given a full stack
+	stack->top = MAX_STACK_SIZE - 1;
+
+	//when
+	bool result = stack_push(stack, 5);
+
+
+	//then
+	TEST_ASSERT_FALSE_MESSAGE(result, "stack_push returned true for full stack");
+	TEST_ASSERT_EQUAL_INT(MAX_STACK_SIZE - 1, stack->top);
+	TEST_ASSERT_NOT_EQUAL(5, stack->data[MAX_STACK_SIZE - 1]);
+}
+
 int main(void)
 {
    UnityBegin("MyStack");
 
    RUN_TEST(test_stack_create);
+   RUN_TEST(test_stack_push_onStackWithOneElement);
+   RUN_TEST(test_stack_push_stackIsFull);
 
    UnityEnd();
    return 0;
